834b68a1c93159c6cd49a4b0d0961ecb25089b88
Cheninja <103942096+Cheninja@users.noreply.github.com>
2022-04-22 10:37:53 +0300

    Add files via upload

---
+++ test_fixture3.py
@@ -0,0 +1,29 @@
+import pytest
+import time
+from selenium import webdriver
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    # этот код выполнится после завершения теста
+    print("\nquit browser..")
+    time.sleep(5)
+    browser.quit()
+
+
+class TestMainPage1():
+    # вызываем фикстуру в тесте, передав ее как параметр
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector("#login_link")
+        time.sleep(3)
+
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+        time.sleep(3)
